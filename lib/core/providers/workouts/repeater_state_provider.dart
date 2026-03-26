d86aa0695b37a9416cc6a2d7f997b032308135eb
corin pearce <corin@pearce-web.co.uk>
2026-03-26 01:54:19 -0400

    refactor(providers): removed useless provider

--- lib/core/providers/workouts/repeater_state_provider.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:lifter/features/workouts/engines/repeater_engine.dart';
-import 'package:lifter/features/workouts/models/repeater_state.dart';
-
-final repeaterEngineProvider = NotifierProvider.autoDispose<RepeaterEngine, RepeaterState>(
-  () => RepeaterEngine(),
-);
